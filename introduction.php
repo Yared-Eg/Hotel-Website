ee0c455654d0c0672386c09b2531d466a69c06f5
Yared-Eg <73594202+Yared-Eg@users.noreply.github.com>
2022-05-21 11:17:49 +0300

    Add files via upload

---
+++ introduction.php
@@ -0,0 +1,27 @@
+<?php include 'header.php';?>
+<div class="container">
+
+       <h1 class="title">Introduction</h1>
+       <div class="row">
+              <div class="col-sm-4"><p>Berlottue Hotel
+
+Berlottue Hotel is a distinct addition to Addis Ababa and a smart choice for travelers. Whether you are traveling for business or pleasure, get the most out of your trip with a stay at Berlottue Hotel.
+Berlottue Hotel offers numerous on-site facilities to satisfy even the most discerning guest. Free internet access provided within the hotel keeps you connected throughout your stay. Airport transfer services can be booked before your check-in date, to ensure a smooth and convenient arrival or departure..</p></div>
+              <div class="col-sm-4"><p>
+For health reasons, smoking is not permitted anywhere inside the hotel. For guests who would like to smoke, the designated smoking areas are available.
+All guestrooms at Berlottue Hotel provide guests with a range of amenities to ensure a restful night. Enjoy your stay even more at the hotel, knowing that some selected rooms include air conditioning. Some rooms at Berlottue Hotel are available with design features like a balcony or terrace. For a bit of entertainment, guests will find television and cable TV available in some selected rooms.
+Don't worry about being thirsty as a coffee or tea maker, instant tea and mini bar are provided in some guestrooms.</p></div>
+              <div class="col-sm-4"><p>Every day at Berlottue Hotel begins with a delicious complimentary breakfast. A number of great dining options at the hotel assures that you'll always have convenient and delicious choices. A great night out couldn't be easier! Enjoy a fun evening without traveling outside at the bar.
+Berlottue Hotel offers unique recreational opportunities for all guests. Burn off you vacation calories with a quick visit to the hotel's fitness facility..</p></div>              
+       </div>
+
+
+       <div class="spacer">
+       <div class="embed-responsive embed-responsive-16by9"><iframe  class="embed-responsive-item" src="//player.vimeo.com/video/55057393?title=0" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+       </div>
+
+
+
+
+</div>
+<?php include 'footer.php';?>
